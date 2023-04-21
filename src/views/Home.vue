2fca85ec62f2bd0d9abe13545ed04f5a78fb6891
taotao <1985504469@qq.com>
2023-04-21 22:36:59 +0800

    Home 页面的 css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@ import Topnav from '../components/Topnav.vue'
 
 <template>
   <div>
-    <Topnav />
+    <Topnav/>
     <div class="banner">
       <h1>轮子UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -16,5 +16,27 @@ import Topnav from '../components/Topnav.vue'
   </div>
 </template>
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
 
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
