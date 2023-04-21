fa3e587c30d8312609f68c4fb01208ad6bf1d488
taotao <1985504469@qq.com>
2023-04-21 21:25:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import Tao from './components/Tao.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tao
+  }
+}
+</script>
+
+<template>
+  <div>hi</div>
+  <Tao />
+</template>
