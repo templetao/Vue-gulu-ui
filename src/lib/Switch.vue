257ac3a416d9eef329b67da7b86e67dd130839d4
taotao <1985504469@qq.com>
2023-04-22 20:11:50 +0800

    可以接受 value、input 事件

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,19 +1,20 @@
 <script lang="ts">
-import {ref} from 'vue'
 
 export default {
-  setup() {
-    const checked = ref(false)
+  props: {
+    value: Boolean
+  },
+  setup(props: any, context: any) {
     const toggle = () => {
-      checked.value = !checked.value
+      context.emit('input', !props.value)
     }
-    return {checked, toggle}
+    return {toggle}
   }
 }
 </script>
 
 <template>
-  <button @click="toggle" :class="{checked}"><span></span></button>
+  <button @click="toggle" :class="{checked:value}"><span></span></button>
 </template>
 
 <style lang="scss" scoped>
