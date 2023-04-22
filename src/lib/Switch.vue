b00d2f5e6f5589eb9fad15e9afcdd4ab092cfa18
taotao <1985504469@qq.com>
2023-04-22 19:55:00 +0800

    hover 之后右滚

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -9,7 +9,7 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: #50C878;
   border-radius: calc($h / 2);
   position: relative;
 }
@@ -22,5 +22,9 @@ span {
   width: $h2;
   background: white;
   border-radius: calc($h / 2);
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
 }
 </style>
