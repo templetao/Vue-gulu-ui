1d2f1c3c5a0103e7dcf63935c7c06413e51c8419
taotao <1985504469@qq.com>
2023-04-24 00:05:09 +0800

    加载中 css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,6 +18,10 @@ export default {
     disabled: {
       type: Boolean,
       default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   setup(props: any) {
@@ -36,6 +40,7 @@ export default {
 
 <template>
   <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading"  class="gulu-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -172,6 +177,25 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .gulu-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $green $green $green transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
+}
+@keyframes gulu-spin {
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
   }
 }
 </style>
