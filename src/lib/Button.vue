50187ed8dfe3897eb79a99c9232d29d065041988
taotao <1985504469@qq.com>
2023-04-23 23:22:29 +0800

    button 基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,7 +16,41 @@ export default {
 </template>
 
 <style lang='scss' scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$green: #50C878;
+$radius: 10px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:focus {
+    color: $green;
+    border-color: $green;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style>
