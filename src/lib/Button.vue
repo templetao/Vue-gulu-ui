7dd8edcdf77881a0ed8d3ea1719c077a64f2797b
taotao <1985504469@qq.com>
2023-04-23 22:59:37 +0800

    创建 button 组件

---
+++ src/lib/Button.vue
@@ -0,0 +1,24 @@
+<script lang='ts'>
+export default {
+  inheritAttrs: false,
+  props: {},
+  setup(props: any, context: any) {
+    const {size, ...rest} = props
+    return {}
+  }
+}
+</script>
+
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+div {
+  border: 1px solid red;
+}
+</style>
