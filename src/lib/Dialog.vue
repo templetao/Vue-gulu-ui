5ed0ce9333a73c9308bf5daa343c10b69593aec5
taotao <1985504469@qq.com>
2023-04-24 21:29:05 +0800

    创建 Dialog 组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,26 @@
+<script lang='ts'>
+import Button from './Button.vue'
+
+export default {
+  components: {Button},
+}
+</script>
+
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行</p>
+      <p>第二行</p>
+    </main>
+    <footer>
+      <Button>Ok</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<style lang='scss'>
+
+</style>
