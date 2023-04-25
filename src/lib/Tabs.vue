0f43bb601bf487a6b2d654ef2822d09eac766581
taotao <1985504469@qq.com>
2023-04-26 00:25:56 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index"
-           @click="select(t)" :class="{selected: t === selected}">{{ t }}
-        <div class="gulu-tabs-nav-indicator"></div>
-      </div>
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles"
+           :ref="el => {if(el) navItems[index] =el}" :key="index"
+           @click="select(t)" :class="{selected: t === selected}">{{ t }}</div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component :is="current" :key="current.props.title"/>
@@ -14,7 +14,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import {computed} from 'vue'
+import {computed, onMounted, onUpdated, ref} from 'vue'
 
 export default {
   props: {
@@ -23,6 +23,22 @@ export default {
     }
   },
   setup(props: any, context: any) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>()
+    const container = ref<HTMLDivElement>()
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value!.style.width = width + 'px'
+      const {left: left1} = container.value!.getBoundingClientRect()
+      const {left: left2} = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value!.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
+
     const defaults = context.slots.default()
     defaults.forEach((tag: any) => {
       if (tag.type !== Tab) {
@@ -39,7 +55,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
-      defaults, titles, current, select
+      defaults, titles, current, select, navItems, indicator, container
     }
   }
 }
@@ -78,6 +94,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
 
