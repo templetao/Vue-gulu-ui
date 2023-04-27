fae5d3ebb9bef235c4b5c7af05ef56fdeec29ea5
taotao <1985504469@qq.com>
2023-04-27 20:30:14 +0800

    解决 npm run dev 出现 bug

--- plugins/md.ts
+++ plugins/md.ts
@@ -3,6 +3,19 @@ import path from 'path'
 import fs from 'fs'
 import marked from 'marked'
 
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 请注意，当前文件的后缀从 .js 改为了 .ts
+// 如果你看到这行注释，请确认文件后缀是 .ts
+// 然后就可以删掉本注释了!!!!!!!!!!!!!!!!
+
+
+
+
+
+
+
 const mdToJs = str => {
   const content = JSON.stringify(marked(str))
   return `export default ${content}`
@@ -25,7 +38,7 @@ export function md() {
     ],
     transforms: [{  // 用于 rollup // 插件
       test: context => context.path.endsWith('.md'),
-      transform: ({ code }) => mdToJs(code)
+      transform: ({ code }) => mdToJs(code) 
     }]
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-// @ts-ignore
-import { defineConfig } from 'vite'
-// @ts-ignore
-import vue from '@vitejs/plugin-vue'
-// @ts-ignore
+// @ts-nocheck
+
 import { md } from "./plugins/md";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), md()],
-})
+export default {
+  plugins: [md()]
+};
